d6b588d1cf77d6c8fc52ed788ea738d3f52b002f
oleh <olegmoroz93@gmail.com>
2021-12-01 18:39:38 +0200

    last

--- db/connection.js
+++
@@ -1,8 +0,0 @@
-const config = require('config');
-const mongoose = require('mongoose');
-
-const connect = async () => {
-  await mongoose.connect(config.get('mongoDB'));
-};
-
-module.exports = { connect };
--- server.js
+++ server.js
@@ -19,6 +19,13 @@ app.use('/api/posts', postRoute);
 app.use('/redirect', express.static('public'));
 app.use('/api', authRoute);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.get('/', (req, res) => {
   res.redirect('/redirect');
 });
